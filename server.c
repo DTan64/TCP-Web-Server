2a3d8e4f4c8cd2f8c55165093115145f66e27cb5
D'Artagnan Wake <dwake70@gmail.com>
2018-10-18 21:09:28 -0600

    works except for gifs

--- server.c
+++ server.c
@@ -31,8 +31,16 @@ int main()
 	char* method;
 	char* splitInput;
 	char fileName[MAXBUFSIZE];
+	char folderName[MAXBUFSIZE];
+	char filePath[MAXBUFSIZE];
+	char fileType[MAXBUFSIZE];
+	char uri[MAXBUFSIZE];
 	int fd;
+	DIR* dir;
+	struct dirent* in_file;
 	int len;
+	int i;
+	int typeIndex;
 
 	char png_response[] = "HTTP/1.1 200 OK:\r\n" "Content-Type: image/png; charset=UTF-8\r\n\r\n";
   char gif_response[] = "HTTP/1.1 200 OK:\r\n" "Content-Type: image/gif; charset=UTF-8\r\n\r\n";
@@ -41,7 +49,7 @@ int main()
   char css_response[] = "HTTP/1.1 200 OK:\r\n" "Content-Type: text/css; charset=UTF-8\r\n\r\n";
   char jpg_response[] = "HTTP/1.1 200 OK:\r\n" "Content-Type: image/jpeg; charset=UTF-8\r\n\r\n";
   char js_response[] = "HTTP/1.1 200 OK:\r\n" "Content-Type: text/javascript; charset=UTF-8\r\n\r\n";
-
+	char htm_response[] = "HTTP/1.1 200 OK:\r\n" "Content-Type: text/html; charset=UTF-8\r\n\r\n";
 
 	/******************
 	  This code populates the sockaddr_in struct with
@@ -75,54 +83,131 @@ int main()
 	int client_length = sizeof(client_addr);
 	bzero(&client_addr, client_length);
 
-  // Accept Connection
-  connectionSock = accept(sock, (struct sockaddr *)&client_addr, (socklen_t *) &client_length);
+	while(1) {
+		// Accept Connection
+	  connectionSock = accept(sock, (struct sockaddr *)&client_addr, (socklen_t *) &client_length);
 
-  if (connectionSock < 0) {
-      perror("ERROR on accept");
-      exit(1);
-   }
+	  if (connectionSock < 0) {
+	      perror("ERROR on accept");
+	      exit(1);
+	   }
 
-   /* If connection is established then start communicating */
-	 bzero(buffer,sizeof(buffer));
-   nbytes = read(connectionSock, buffer, MAXBUFSIZE);
+	   /* If connection is established then start communicating */
+		 bzero(buffer,sizeof(buffer));
+		 //bzero(method,sizeof(method));
+		 //bzero(splitInput,sizeof(splitInput));
+		 bzero(fileName,sizeof(fileName));
+		 bzero(folderName,sizeof(folderName));
+		 bzero(filePath,sizeof(filePath));
+		 bzero(fileType,sizeof(fileType));
+		 bzero(uri,sizeof(uri));
+	   nbytes = read(connectionSock, buffer, MAXBUFSIZE);
 
-   if (nbytes < 0) {
-      perror("ERROR reading from socket");
-      exit(1);
-   }
+	   if (nbytes < 0) {
+	      perror("ERROR reading from socket");
+	      exit(1);
+	   }
 
-   printf("%s\n",buffer);
+	   printf("%s\n",buffer);
 
-	 // GET or POST
-	 method = strtok(buffer, " ");
+		 // GET or POST
+		 method = strtok(buffer, " ");
+		 if(strcmp(method, "GET")) {
+			 return -5;
+		 }
 
-	 // Get filename
-	 splitInput = strtok(NULL, " ");
-	 //fileName = strtok(NULL, " ");
-	 if (splitInput[0] == '/') splitInput++;
-	 sprintf(fileName, "%s", splitInput);
-	 //splitInput[strlen(splitInput) - 1] = '\0';
+		 // Get filename
+		 splitInput = strtok(NULL, " ");
+		 printf("request: %s\n", splitInput);
+		 sprintf(uri, "%s", splitInput);
+		 if(!strcmp(splitInput, "/") || !strcmp(splitInput, "/index.html")) {
+			 printf("hit\n");
+			 len = send(connectionSock, html_response, sizeof(html_response) - 1, 0);
+			 bzero(buffer,sizeof(buffer));
+			 fd = open("index.html", O_RDONLY);
+			 while(read(fd, buffer, sizeof(buffer)) != 0) {
+				 len = send(connectionSock, buffer, strlen(buffer), 0);
+				 bzero(buffer,sizeof(buffer));
+			 }
+
+		 }
+		 else {
+
+			 if (splitInput[0] == '/') splitInput++;
+			 sprintf(filePath, "%s", splitInput);
+			 splitInput = strtok(filePath, "/");
+			 sprintf(folderName, "%s", splitInput);
+			 splitInput = strtok(NULL, "/");
+			 sprintf(fileName, "%s", splitInput);
+
+			 int uriLen = strlen(uri);
+			 const char *last_four = &uri[uriLen-4];
+			 printf("tyep?: %s\n", last_four);
 
 
-	 printf("%s\n", splitInput);
-	 //fd = open(filename, O_RDONLY);
 
-	 //nbytes = send(sock, &over, sizeof(over), 0, (struct sockaddr *) &remote, sizeof(remote));
+			 for(i = uriLen; i >= 0; i--) {
+				 if(!strncmp(&uri[i], ".", 1)) {
+					 typeIndex = i;
+					 printf("hit\n");
+					 break;
+				 }
+			 }
 
-	 len = send(connectionSock, html_response, sizeof(html_response) - 1, 0);
+			 sprintf(fileType, "%s", &uri[typeIndex + 1]);
+			 printf("folderName: %s\n", folderName);
+			 printf("fileName: %s\n", fileName);
+			 printf("fileType: %s\n", fileType);
+			 printf("uri: %s\n", uri);
 
-	 bzero(buffer,sizeof(buffer));
-	 fd = open(fileName, O_RDONLY);
-	 while(read(fd, buffer, sizeof(buffer)) != 0) {
-		 printf("hit\n" );
-	   len = send(connectionSock, buffer, strlen(buffer), 0);
-	 	 bzero(buffer,sizeof(buffer));
-	 }
+			 bzero(filePath,sizeof(filePath));
+			 strcat(filePath, ".");
+			 strcat(filePath, uri);
+			 printf("filePath: %s\n", filePath);
 
 
-	 //len = send(connectionSock, &fd, sizeof(fd) - 1, 0);
-	 printf("%i\n", len);
+			if(!strcmp(fileType, "png")) {
+			 len = send(connectionSock, png_response, sizeof(png_response) - 1, 0);
+			}
+			else if(!strcmp(fileType, "gif")) {
+			 len = send(connectionSock, gif_response, sizeof(gif_response) - 1, 0);
+			}
+			else if(!strcmp(fileType, "html")) {
+			 len = send(connectionSock, html_response, sizeof(html_response) - 1, 0);
+			}
+			else if(!strcmp(fileType, "txt")) {
+			 len = send(connectionSock, text_response, sizeof(text_response) - 1, 0);
+			}
+			else if(!strcmp(fileType, "css")) {
+			 len = send(connectionSock, css_response, sizeof(css_response) - 1, 0);
+			}
+			else if(!strcmp(fileType, "jpg")) {
+			 len = send(connectionSock, jpg_response, sizeof(jpg_response) - 1, 0);
+			}
+			else if(!strcmp(fileType, "js")) {
+			 len = send(connectionSock, js_response, sizeof(js_response) - 1, 0);
+			}
+			else if(!strcmp(fileType, "htm")) {
+			 len = send(connectionSock, htm_response, sizeof(htm_response) - 1, 0);
+			}
+
+			 bzero(buffer,sizeof(buffer));
+			 fd = open(filePath, O_RDONLY);
+			 if(fd < 0) {
+				 printf("Error opening file.\n");
+				 return -1;
+			 }
+
+			 while(read(fd, buffer, sizeof(buffer)) != 0) {
+				 printf("sending...\n");
+				 printf("%s\n", buffer);
+				 len = send(connectionSock, buffer, strlen(buffer), 0);
+				 bzero(buffer,sizeof(buffer));
+			 }
+
+		 }
+	}
+
 	 close(sock);
 
 }
